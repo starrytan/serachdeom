10819a99a45e465a723f79f4cb91452a8c66abe6
tanjie <384759564@qq.com>
2019-11-03 11:34:43 +0800

    up

--- src/view/details/details.jsx
+++ src/view/details/details.jsx
@@ -5,13 +5,14 @@ import Imgbox from "../components/imgbox";
 import common from "../../static/jsx/common";
 import {BackTop,Icon} from "antd";
 import imgup from '../../static/img/up_arrow.svg';
-import imgdown from '../../static/img/down_arrow.svg';
+import './no.css'
 class Details extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       imgurl: "",
-      data:JSON.parse(localStorage.getItem('listdata'))
+      data:JSON.parse(localStorage.getItem('listdata')),
+      visibility:true
     };
   }
   viewimg = (item) => {
@@ -33,13 +34,13 @@ class Details extends React.Component {
     let target = e.target.parentNode
     if(target.tagName=='B'){
       if(target.nextSibling.style.maxHeight=='0px'||!target.nextSibling.style.maxHeight){
-        target.lastChild.src=imgdown;
+        target.lastChild.style.transform = 'rotate(0deg)'
         let myheight = target.nextSibling.getAttribute('myheight');
         target.nextSibling.style.maxHeight =myheight+'px'
 
       }else{
-        target.lastChild.src=imgup;
-        target.nextSibling.style.maxHeight = '0px'
+        target.nextSibling.style.maxHeight = '0px';
+        target.lastChild.style.transform = 'rotate(-90deg)'
       }
     }
   }
@@ -58,16 +59,17 @@ class Details extends React.Component {
             <p><b>{this.state.data.title}</b></p>
             <p className={styles.titlename}>章节：</p>
             <p><b>{this.state.data.capter}</b></p>
-            <p className={styles.titlename}>小节：</p>
-            <p><b>{this.state.data.section}</b></p>
-            <p className={styles.titlename}>内容：</p>
+            <p className={styles.titlename}>简介：</p>
             <div
               onClick={this.flexible}
               id="content"
+              style={this.state.visibility?{visibility:'hidden'}:{}}
               className={styles.content}
               dangerouslySetInnerHTML={{
                 __html: this.state.data.content
               }}></div>
+            <p className={styles.titlename}>小节：</p>
+            <p><b>{this.state.data.section}</b></p>
           </div>
           <div className={styles.rightbox}>
             {this.state.data.urls?this.state.data.urls.map((item,index)=>{
@@ -100,8 +102,9 @@ class Details extends React.Component {
     let reg = /<div id=\"t\d\">/;
     let idreg = /t\d/;
     let data = this.state.data;
-    data.content=data.content.replace(/<\/b>/g,'<img style="width:20px;margin-left:20px" src="'+imgup+'"/></b>');
-    data.content=data.content.replace(/<span/g,'<span class="mainbox"');
+    data.content=data.content.replace(/<\/b>/g,'<img style="width:15px;transform:rotate(-90deg);transition:transform .3s" src="'+imgup+'"/></b>');
+    data.content=data.content.replace(/<span>/g,'<span class="mainbox">');
+    data.content=data.content.replace(/<span class="sub"/g,'<span class="mainbox sub"');
     let imgarr = data.content.match(reg);
     if(imgarr instanceof Array){
       imgarr.map(item => {
@@ -116,11 +119,17 @@ class Details extends React.Component {
     }
   }
   componentDidMount() {
-    let mainbox = document.getElementsByClassName('mainbox');
-    for(let box of mainbox){
-      box.setAttribute('myheight',box.clientHeight+20);
-      // box.style.maxHeight='0px'
-    }
+    setTimeout(()=>{
+      let mainbox = document.getElementsByClassName('mainbox');
+      for(let box of mainbox){
+        box.setAttribute('myheight',box.clientHeight+20);
+        box.style.maxHeight='0px'
+      }
+      this.setState({
+        visibility:false
+      })
+    },100)
+
     let domarr = document.getElementsByClassName("lazyload");
     for (let dom of domarr) {
       common.lazyload(dom);
