a173a8452be08873f59c3d16fd3968df25fa02b0
acproject <acproject@qq.com>
2019-11-04 17:27:42 +0800

    update WSIBox

--- src/view/components/wsibox.jsx
+++ src/view/components/wsibox.jsx
@@ -3,10 +3,35 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+
+const OPENSILDE_INFO_REQUEST = "?action=info&path=";
+const OPENSILDE_THUMBNAIL_REQUEST = "?action=image&path=";
+const OPENSILDE_REGION_REQUEST = "?action=region&path=";
+//声明一些全局的对象来处理数据
+
+var sourceImages;
+var sourceProperties;
+var sourceCase;
+
+var currentImage;
+
+var snapshotDiv = "snapshot";
+
+var ourGestureSettingsMouse = {
+    
+};
+
 const WSIBox = (wsiurl) => {
     class WSIBox extends React.Component {
+        
+
         constructor(props) {
             super(props);
+            this.state = {
+                sourceImages:sourceImages,
+                sourceProperties:sourceProperties,
+                sourceCase:sourceCase,
+            }
         }
 
         componentDidMount() {
