bf643731dcb3d4f48c3fe5951109b54ff83ccef5
tanjie <384759564@qq.com>
2020-02-06 20:56:54 +0800

    1

--- src/view/components/register.jsx
+++ src/view/components/register.jsx
@@ -39,6 +39,7 @@ class Register extends React.Component {
                 rules: [{ required: true, message: "请输入手机号" }]
               })(
                 <Input
+                  name="phone"
                   placeholder="请输入手机号"
                   addonBefore={prefixSelector}
                   style={{ width: "100%" }}
@@ -48,12 +49,24 @@ class Register extends React.Component {
             <Form.Item>
               {getFieldDecorator("password", {
                 rules: [{ required: true, message: "请输入密码" }]
-              })(<Input type="password" placeholder="请输入密码" />)}
+              })(
+                <Input
+                  name="password"
+                  type="password"
+                  placeholder="请输入密码"
+                />
+              )}
             </Form.Item>
             <Form.Item>
               {getFieldDecorator("repassword", {
                 rules: [{ required: true, message: "请确认密码" }]
-              })(<Input type="password" placeholder="请再次输入密码" />)}
+              })(
+                <Input
+                  name="repassword"
+                  type="password"
+                  placeholder="请再次输入密码"
+                />
+              )}
             </Form.Item>
             <Form.Item>
               <div className="flexbet">
@@ -61,6 +74,7 @@ class Register extends React.Component {
                   rules: [{ required: true, message: "请输入验证码" }]
                 })(
                   <Input
+                    name="code"
                     style={{ marginRight: "10px" }}
                     type="number"
                     placeholder="请输入验证码"
--- src/view/components/sendcode.jsx
+++ src/view/components/sendcode.jsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {Button } from "antd";
 const SendCode=()=>{
-    const [count, setCount] = useState(60);
+    const [count, setCount] = useState(60); 
+    const sendcode = ()=>{
+      let num = count;
+      let countdown = setInterval(() => {
+        num = num-1;
+        if (num == 0) {
+          clearInterval(countdown);
+          setCount(60);
+        } else {
+          setCount(num - 1);
+        }       
+      }, 1000);
+    };
     return (
-      <div>
+      <div style={{ width: "102px", textAlign: "center" }}>
         {count == 60 ? (
           <Button onClick={sendcode}>发送验证码</Button>
         ) : (
-          <div>{count}s</div>
+          <div style={{ color: "#40a9ff" }}>{count}s</div>
         )}
       </div>
     );
